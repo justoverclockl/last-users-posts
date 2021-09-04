2a302187a74d4274e4ec0495d49b61af6139363c
Marco Colia <info@justoverclock.it>
2021-09-04 17:39:07 +0200

    fixed for guests

--- js/src/common/components/LastUsersPosts.js
+++ js/src/common/components/LastUsersPosts.js
@@ -11,63 +11,66 @@
 import Widget from 'flarum/extensions/afrux-forum-widgets-core/common/components/Widget';
 import app from 'flarum/forum/app';
 import LoadingIndicator from 'flarum/common/components/LoadingIndicator';
-// import Separator from 'flarum/common/components/Separator';
 import { truncate } from 'flarum/common/utils/string';
 
 export default class EventsWidget extends Widget {
-    oninit(vnode) {
-        super.oninit(vnode);
-        this.loading = true;
-    }
+  oninit(vnode) {
+    super.oninit(vnode);
+    this.loading = true;
+  }
 
-    oncreate(vnode) {
-      // settings to limit post number on frontend
-        const limitPost = app.forum.attribute('justoverclock-last-users-posts.postLimit') || 10;
-      // get events object
-        const lastpwidget = app.store
-            .find('posts', {
-                isApproved: true,
-                sort: '-createdAt',
-                page: { limit: limitPost },
-            })
-            .then((results) => {
-                this.post = results;
-                // console.log(results); <---debug line
-                this.loading = false;
-                m.redraw();
-            });
-    }
+  oncreate(vnode) {
+    // settings to limit post number on frontend
+    const limitPost = app.forum.attribute('justoverclock-last-users-posts.postLimit') || 10;
+    // get posts json
+    const lastpwidget = app.store
+      .find('posts', {
+        isApproved: true,
+        exists: true,
+        sort: '-createdAt',
+        page: { limit: limitPost },
+        filter: { type: 'comment' }
+      })
+      .then((results) => {
+        this.post = results;
+        // console.log(results); <---debug line
+        this.loading = false;
+        m.redraw();
+      });
+  }
 
-    className() {
-         // css class for the container
-        return 'lastposts-widget';
-    }
+  className() {
+    // css class for the container
+    return 'lastposts-widget';
+  }
 
-    icon() {
-        // Widget icon.
-        return 'fas fa-edit';
-    }
+  icon() {
+    // Widget icon.
+    return 'fas fa-edit';
+  }
 
-    title() {
-        // Widget title.
-        return app.translator.trans('justoverclock-last-users-posts.forum.widget-title');
-    }
+  title() {
+    // Widget title.
+    return app.translator.trans('justoverclock-last-users-posts.forum.widget-title');
+  }
 
-    content() {
-        if (this.loading) {
-            return <LoadingIndicator />;
-        }
-        const Charlength = app.forum.attribute('justoverclock-last-users-posts.CharLength') || 80;
-        return (
-            <div className="last-posts-content">
-                <ul className="lastpostwidget fa-ul">
-                    {this.post.map((post) => (
-                        <p class="lastpostwdg"><i class="fas fa-user-edit postwdgicon"></i>
-                            {truncate(post.content(), Charlength, 0)}
-                        </p>
-                    ))}
-                </ul>
-            </div>
-        );
+  content() {
+    if (this.loading) {
+      return <LoadingIndicator />;
     }
+    const Charlength = app.forum.attribute('justoverclock-last-users-posts.CharLength') || 80;
+    return (
+      <div className="last-posts-content">
+        <ul className="lastpostwidget fa-ul">
+          {this.post.map((post) => {
+            return (
+              <p class="lastpostwdg"><i class="fas fa-user-edit postwdgicon"></i>
+                {truncate(post.contentHtml().replace(/<\/?[^>]+(>|$)/g, ""), Charlength, 0)}
+              </p>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
